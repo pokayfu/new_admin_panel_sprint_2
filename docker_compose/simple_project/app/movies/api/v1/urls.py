0ec26d113a0657b2be330eca3b73739f49fb70c9
Grigoriy <pokayyfu@gmail.com>
2024-04-26 14:18:40 +0300

    finally

--- docker_compose/simple_project/app/movies/api/v1/urls.py
+++ docker_compose/simple_project/app/movies/api/v1/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from movies.api.v1 import views
 
+
 urlpatterns = [
-    path('movies/', views.api),
+    path("movies/", views.MoviesListApi.as_view()),
+    path("movies/<uuid:id>/", views.MoviesDetailApi.as_view()),
 ]
