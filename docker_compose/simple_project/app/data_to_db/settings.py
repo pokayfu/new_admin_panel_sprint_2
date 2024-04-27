4437ef3d39ba94104ac70b655cfcbb8891f82e28
Grigoriy <pokayyfu@gmail.com>
2024-04-27 10:38:57 +0300

    removed dummy files and folders

--- docker_compose/simple_project/app/data_to_db/settings.py
+++
@@ -1,9 +0,0 @@
-from credentials import dsn_creds
-
-dsn = {'dbname': dsn_creds['db_name'],
-        'user': dsn_creds['db_user'],
-        'password': dsn_creds['db_pswd'],
-        'host': dsn_creds['host'],
-        'port': dsn_creds['port']}
-
-sql_path = 'db.sqlite'
--- docker_compose/simple_project/app/setup_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd data_to_db && python main.py && cd ..
-
-python manage.py migrate
-
-uwsgi --ini uwsgi.ini
