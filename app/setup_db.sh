41c0b3dc310ad5a4a12936d71f3a0d6ab34b956c
Grigoriy <pokayyfu@gmail.com>
2024-04-27 16:26:12 +0300

    fixed config

--- app/setup_db.sh
+++ app/setup_db.sh
@@ -3,8 +3,11 @@
 cd data_to_db && python main.py && cd ..
 
 python manage.py collectstatic --no-input
+
 python manage.py migrate
+
 python manage.py compilemessages -l en -l ru 
+
 python manage.py createsuperuser --noinput || true
 
 uwsgi --strict  --ini  uwsgi.ini
